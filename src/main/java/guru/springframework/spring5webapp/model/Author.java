0d04b2359d1ba438cb95c6d713df94ce8d8f98e6
Eugeniu Serbaniuc <eserbaniuc@costengineering.eu>
2020-01-26 23:54:49 +0200

    adding JPA example

---
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -0,0 +1,42 @@
+package guru.springframework.spring5webapp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+
+/* eserbaniuc created on 01/26/2020 */
+@Entity
+@Data
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+}
