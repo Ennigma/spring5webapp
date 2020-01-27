1457edfe9720b08881a36544908fa3cd2b139575
Eugeniu Serbaniuc <eserbaniuc@costengineering.eu>
2020-01-27 13:40:54 +0200

    adding-thymeleaf

--- src/main/java/guru/springframework/spring5webapp/controller/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controller/BookController.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5webapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import guru.springframework.spring5webapp.repositories.BookRepository;
 
@@ -15,9 +16,10 @@ public class BookController {
         this.bookRepository = bookRepository;
     }
 
+    @RequestMapping("/books")
     public String getBooks(Model model) {
         model.addAttribute("books", bookRepository.findAll());
 
-        return "book";
+        return "books";
     }
 }
